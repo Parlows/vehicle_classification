58c32ef92a800f1c88d282ebb9a25a0c3acba0d2
Parlows <pablo.regodon@hotmail.com>
2022-02-17 13:40:01 +0100

    Organized data and downloaded resnet50

---
+++ python_modules/data_organization.py
@@ -0,0 +1,52 @@
+"""
+
+This is a module that organizes the different sets in the dataset
+
+Author: Pablo Regodón Cerezo
+Date: February 2022
+
+"""
+
+# Setup
+import os
+import shutil
+import random
+import glob
+from progress_bar import print_progress_bar
+
+dataset_path = '../partial_ds/'
+
+# Organize sets
+os.makedirs(dataset_path + 'valid/Car')
+os.makedirs(dataset_path + 'valid/Bus')
+os.makedirs(dataset_path + 'valid/Truck')
+os.makedirs(dataset_path + 'valid/Van')
+os.makedirs(dataset_path + 'valid/Motorcycle')
+os.makedirs(dataset_path + 'test/Car')
+os.makedirs(dataset_path + 'test/Truck')
+os.makedirs(dataset_path + 'test/Bus')
+os.makedirs(dataset_path + 'test/Van')
+os.makedirs(dataset_path + 'test/Motorcycle')
+
+# Take a portion of original ds
+for categorie in os.listdir(dataset_path + 'train/'):
+	
+	print(f"Building test ds for {categorie}")
+	i = 0
+	print_progress_bar(i, 50, prefix=' Building test ds:', suffix='complete', length=50)
+	# 50 images for testing
+	for image_file in random.sample(glob.glob(dataset_path + 'train/' + categorie + '/*'), 50):
+		shutil.move(image_file, dataset_path + 'test/' + categorie + '/')
+		i+=1
+		print_progress_bar(i, 50, prefix=' Building test ds:', suffix='complete', length=50)
+	
+	# 20% of the rest for validation
+	n_validation = int(len(os.listdir(dataset_path + 'train/' + categorie)) * 0.2)
+	print(f"Building validation ds for {categorie}")
+	i = 0
+	print_progress_bar(i, n_validation, prefix=' Building validation ds:', suffix='complete', length=50)
+	for image_file in random.sample(glob.glob(dataset_path + 'train/' + categorie + '/*'), n_validation):
+		shutil.move(image_file, dataset_path + 'valid/' + categorie + '/')
+		i+=1
+		print_progress_bar(i, n_validation, prefix=' Building validation ds:', suffix='complete', length=50)
+
--- python_modules/resnet50.py
+++ python_modules/resnet50.py
@@ -17,44 +17,87 @@ Date: February 2022.
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import os
 
-# Generate training and validation datasets
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-	directory="../partial_ds/train/train", # Directory of the dataset
-	batch_size=32, # Batch size
-	image_size=(224, 224), # Input image size
-	seed=1337, # Random seed to generate the split
-	validation_split=0.2, # 20% of set for validation
-	subset="training", # This will be the training subset
-)
+# Set the different datasets paths
+train_path = '../partial_ds/train'
+valid_path = '../partial_ds/valid'
+test_path = '../partial_ds/test'
 
-validation_ds = tf.keras.preprocessing.image_dataset_from_directory(
-	directory="../partial_ds/train/train", # Directory of the dataset
-	batch_size=32, # Batch size
-	image_size=(224, 224), # Input image size
-	seed=1337, # Random seed to generate the split
-	validation_split=0.2, # 20% of set for validation
-	subset="validation", # This will be the validating subset
-)
+# Load the sets
+train_batches = ImageDataGenerator(tf.keras.applications.resnet50.preprocess_input) \
+	.flow_from_directory(
+			      directory=train_path,
+			      target_size=(224, 224),
+			      classes=['Car','Truck','Bus','Van','Motorcycle'],
+			      batch_size=5
+			     )
 
-# Resize the images
-resizer = tf.keras.Sequential()
-resizer.add(tf.keras.layers.Resizing(224, 224))
+valid_batches = ImageDataGenerator(tf.keras.applications.resnet50.preprocess_input) \
+	.flow_from_directory(
+			      directory=valid_path,
+			      target_size=(224, 224),
+			      classes=['Car','Truck','Bus','Van','Motorcycle'],
+			      batch_size=5
+			     )
 
-# Data augmentation
-data_augmentation = tf.keras.Sequential()
-data_augmentation.add(tf.keras.layers.RandomFlip("horizontal"))
-data_augmentation.add(tf.keras.layers.RandomRotation(0.1))
+test_batches = ImageDataGenerator(tf.keras.applications.resnet50.preprocess_input) \
+	.flow_from_directory(
+			      directory=test_path,
+			      target_size=(224, 224),
+			      classes=['Car','Truck','Bus','Van','Motorcycle'],
+			      batch_size=5,
+			      shuffle=False
+			     )
 
-# Build the model
-model = keras.Sequential(
-	[
-		resizer,
-		data_augmentation,
-		tf.keras.applications.ResNet50(include_top=False, weights="imagenet")
-		tf.keras.layers.Dense(classes=5, activation='softmax', name='fc5')
-	]
-)
+imgs, labels = next(train_batches) # 5 images + 5 labels
+
+"""
+This function plots an array of images passed as a parameter.
+
+"""
+def plotImages(images_arr):
+	fig, axes = plt.subplots(1, 5, figsize=(20, 20))
+	axes = axes.flatten()
+	for img, ax in zip( images_arr, axes):
+		ax.imshow(img)
+		ax.axis('off')
+	plt.tight_layout()
+	plt.show(block=True)
+
+""" For Debbuging
+print(labels)
+plotImages(imgs)
+"""
+
+# Train or load model
+if 'resnet50.h5' in os.listdir('../saved_models/'):
+	model = keras.models.load_model('resnet50.h5')
+	model.summary()
+else:
+	# Download model
+	resnet50_model = tf.keras.applications.resnet50.ResNet50()
+	resnet50_model.summary()
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
