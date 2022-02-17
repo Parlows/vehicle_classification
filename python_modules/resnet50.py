20b16c94e9fd311dd31d33d65d7dced332e99987
Parlows <pablo.regodon@hotmail.com>
2022-02-17 14:00:40 +0100

    Finished coding ResNet50 module

--- python_modules/resnet50.py
+++ python_modules/resnet50.py
@@ -18,6 +18,10 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
 import matplotlib.pyplot as plt
 import os
 
@@ -78,10 +82,63 @@ if 'resnet50.h5' in os.listdir('../saved_models/'):
 	model.summary()
 else:
 	# Download model
-	resnet50_model = tf.keras.applications.resnet50.ResNet50()
+	resnet50_model = tf.keras.applications.ResNet50(
+							  include_top=False,
+							  weights='imagenet',
+							  input_shape=(224, 224, 3),
+							  pooling='avg'
+							 )
 	resnet50_model.summary()
 	
+	x = resnet50_model.layers[-1].output
+	output = Dense(units=5, activation='softmax')(x)
+	
+	model = Model(inputs=resnet50_model.input, outputs=output)
+	
+	model.summary()
+	
+	model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+	model.fit(x=train_batches, validation_data=valid_batches, epochs=10, verbose=2)
+	model.save('../saved_models/resnet50.h5')
 
+# Prediction
+
+def plot_confusion_matrix(cm, classes,
+			   normalize=False,
+			   title='Confusion matrix',
+			   cmap=plt.cm.Blues):
+	
+	plt.imshow(cm, interpolation='nearest', cmap=cmap)
+	plt.title(title)
+	plt.colorbar()
+	tick_marks = np.arange(len(classes))
+	plt.xticks(tick_marks, classes, rotation=45)
+	plt.yticks(tick_marks, classes)
+	
+	if normalize:
+		cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+		print("Normalized confusion matrix")
+	else:
+		print("Confusion matrix, without normalization")
+	
+	print(cm)
+	
+	thresh = cm.max() / 2.
+	for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+		plt.text(j, i, cm[i, j],
+			horizontalalignment="center",
+			color="white" if cm[i, j] > thresh else "black")
+	plt.tight_layout()
+	plt.ylabel("True label")
+	plt.xlabel("Predicted label")
+	plt.show()
+
+predictions = model.predict(x=test_batches, verbose=0)
+
+cm = confusion_matrix(y_true=test_batches.classes, y_pred=np.argmax(predictions, axis=-1))
+
+cm_plot_labels = ['Car', 'Truck', 'Bus', 'Van', 'Motorcycle']
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
 
 
 
